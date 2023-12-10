3ac9ee37c0335a94871162e1dca95cddef7e4a59
cacaomath <mahiro.1192@gmail.com>
2023-12-11 00:00:34 +0900

    feat : getFeedsの処理追加(#7)
    - エラー処理とレスポンス返すように型追加

--- api/RssApi.ts
+++ api/RssApi.ts
@@ -1,19 +1,34 @@
 import axios, { AxiosResponse } from 'axios';
 
+export interface RssApiMethodResponse {
+  result: RssApiRequestResult;
+  response: AxiosResponse | unknown;
+}
+
+export type RssApiRequestResult = 'OK' | 'AXIOS_ERROR' | 'UNEXPECTED_ERROR';
 export class RssApi {
   constructor(url: string) {
     axios.defaults.baseURL = url;
   }
 
-  async getFeeds() {
+  async getFeeds(): Promise<RssApiMethodResponse> {
     try {
       const response: AxiosResponse = await axios.get('feeds');
-      return response;
+      return { result: 'OK', response: response };
     } catch (error) {
       if (axios.isAxiosError(error)) {
+        if (error.response) {
+          console.log('server error');
+          return { result: 'AXIOS_ERROR', response: error.response };
+        } else if (error.request) {
+          console.log('request error');
+          return { result: 'AXIOS_ERROR', response: error.request };
+        }
       }
+      return { result: 'UNEXPECTED_ERROR', response: error };
     }
   }
+
   async postRss(categories: [string]) {
     try {
       const response = await axios.post('/rss', { categories: categories });
