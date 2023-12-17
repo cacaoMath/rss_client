e71f68eb1673e257436d3cf4a5684907af4678ba
cacaomath <mahiro.1192@gmail.com>
2023-12-17 23:21:00 +0900

    feat: tan stack queryを使えるようにする(#7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Menubar from '@/../component/MenuBar';
+import { Provider } from '@/../component/ReactQueryProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <Menubar />
-        {children}
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
