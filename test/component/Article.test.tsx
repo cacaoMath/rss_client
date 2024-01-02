30ea04723d6684b8f458a82cdfbb869e8e4c3c6f
cacaomath <mahiro.1192@gmail.com>
2024-01-02 02:53:44 +0000

    fix: 落ちているテストの修正(#18)

--- test/component/Article.test.tsx
+++ test/component/Article.test.tsx
@@ -1,31 +1,34 @@
 import Article, { ArticleProps } from '@/../component/Article';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { RssData } from '../../api/RssApi';
 
 const testData: ArticleProps = {
-  title: 'title',
-  link: 'https://cacaomath.com',
-  summary: 'this page show cacaomath work',
-  published: '1999/9/9',
+  articleData: {
+    title: 'title',
+    link: 'https://cacaomath.com',
+    summary: 'this page show cacaomath work',
+    published: '1999/9/9',
+  } as RssData,
 };
 
 describe('Article componentのテスト', () => {
   it('tieleの文言が表示されている', () => {
     render(<Article {...testData} />);
-    expect(screen.getByText(testData.title)).toBeInTheDocument();
+    expect(screen.getByText(testData.articleData.title)).toBeInTheDocument();
   });
   it('summaryの文言が表示されている', () => {
     render(<Article {...testData} />);
-    expect(screen.getByText(testData.summary)).toBeInTheDocument();
+    expect(screen.getByText(testData.articleData.summary)).toBeInTheDocument();
   });
   it('publishedの文言が表示されている', () => {
     render(<Article {...testData} />);
-    expect(screen.getByText(testData.published)).toBeInTheDocument();
+    expect(screen.getByText(testData.articleData.published)).toBeInTheDocument();
   });
   it('linkのボタンが存在する', () => {
     render(<Article {...testData} />);
     const linkButton = screen.getByRole('link');
     expect(linkButton.textContent).toEqual('元記事へ');
-    expect(linkButton).toHaveAttribute('href', testData.link);
+    expect(linkButton).toHaveAttribute('href', testData.articleData.link);
   });
 });
